7ada5f24fd6720013f94438df6f5b689c019b278
Andriy Pashynnyk <tobirawork@gmail.com>
2024-07-11 18:21:22 +0300

    fix mask

--- src/mask.ts
+++ src/mask.ts
@@ -5,5 +5,9 @@ export function mask(s: string) {
     return "";
   }
 
+  if (s.length < N * 2) {
+    return "*".repeat(s.length);
+  }
+
   return s.slice(0, N) + "*".repeat(s.length - N * 2) + s.slice(-N);
 }
